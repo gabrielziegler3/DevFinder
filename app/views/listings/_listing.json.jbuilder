d59ca5b5a4b26b2903bd234ca57171784c611095
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-06-16 23:57:37 -0300

    Adding facebook like button and removing listing scaffold files

--- app/views/listings/_listing.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! listing, :id, :name, :skill, :created_at, :updated_at
-json.url listing_url(listing, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170616221543) do
 
-  create_table "listings", force: :cascade do |t|
-    t.string   "name"
-    t.string   "skill"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "company"
