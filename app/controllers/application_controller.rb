1f517f63b5bb2a282a26d05ea771b293ec733921
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-07-01 14:59:19 -0300

    Added name to users and description added to projects show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
         devise_parameter_sanitizer.permit(:sign_up, keys: [:category])
         devise_parameter_sanitizer.permit(:account_update, keys: [:category])
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701165513) do
+ActiveRecord::Schema.define(version: 20170701174523) do
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "name"
+    t.string   "skill"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170701165513) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "category"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
