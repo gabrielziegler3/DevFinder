c32552f5ade51db5f6c60d0b60107b1334758326
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-07-01 14:36:01 -0300

    Created categories to users and linked projects to users

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
-    has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "no-image-found.jpg"
+    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "no-image-found.jpg"
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+    validates :name, :skill, presence: true
+
+
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616221543) do
+ActiveRecord::Schema.define(version: 20170701165513) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170616221543) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170616221543) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "category"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
